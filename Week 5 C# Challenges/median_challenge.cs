37b373107b33e36023a2413fcc5d96cdcca2aa71
lukekavanagh <lukekavanagh1@yahoo.co.uk>
2015-03-09 17:34:45 +1300

    median challenge work in progress

---
+++ Week 5 C# Challenges/median_challenge.cs
@@ -0,0 +1,49 @@
+// Pseudo-code
+// create list called 'median' which takes as its input a list of numbers
+// The median is caLculated by placing numbers in the order of their value, and then finding the middle number
+// To this end, I would imagine a sorting method will have to be used on the list.
+// Median is a form of average - the list will have to be divided by 2
+
+
+// Initial solution
+public static Median (list <int>)
+{
+    list.Sort();
+    foreach (int value in list)
+{
+    list.Average();
+}
+    return median;
+
+
+
+
+// Refactored solution
+
+ public static int Median(List<int> list)
+        {
+            list.Sort();
+
+            if (list.Count % 2 == 0)
+            {
+                int sum = int sum = list[(list.Count / 2) - 1] + list[(list.Count / 2)];
+                return sum / 2;
+            }
+            else
+                return list.Count / 2;
+
+
+
+
+
+/*******Look at Solution*******/
+// Second Refactored solution
+
+
+
+// Reflections
+// To be honest, I had the idea of how to calculate the median and how to sort the list, but got bogged down when googling for help.
+// I spent way too long on this challenge, in some ways, not giving up is one of my good traits but is also one of my bad ones,
+// because I become a real beligerent sod. I think I will give this one another go later in the week as there is a problem with the example solution,
+// I understood what I have to do to pass the tests and managed to pass a couple, but its a case of being able to express what I want to do which is holding me back at the minute.
+// I am getting better at being able to think about lists/  arrays creatively though.
