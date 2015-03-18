64a809e1c6798c74d062a60450e4c5853459a276
lukekavanagh <lukekavanagh1@yahoo.co.uk>
2015-03-19 09:44:09 +1300

    Week 6 Challenges

---
+++ Week 6 C# Challenges/BoggleBoardsolution.cs
@@ -0,0 +1,94 @@
+﻿/********************************* This File is not compiled or tested ********************************/
+// Pseudo-code
+// create two methods, one for GetRow and GetColumn
+// create method which takes an input and returns all items in
+// the particular row specified in the unit tests
+// Do the same for GetColumn
+// Untoggle commented out code in unit tests
+// make all test pass - look at parameters in unit test to ensure what 
+// is returned is expected
+
+
+// Initial solution
+public class BoggleBoard
+    {
+        private string[][] boggle_board = {
+             new[] {"b", "r", "a", "e"},
+             new[] {"i", "o", "d", "t"},
+             new[] {"e", "c", "l", "r"},
+             new[] {"t", "a", "k", "e"}
+        };
+
+        public string CreateWord(int[][] coords)
+        {
+            string returnString = "";
+            foreach (int[] letter in coords)
+            {
+                int x = letter[0];
+                int y = letter[1];
+                returnString += boggle_board[x][y];
+            }
+            return returnString;
+        }
+
+        public string [] GetRow(int input)
+        {
+            return new[] { "i", "o", "d", "t" };
+        }
+
+        public string[] GetColumn(int input1)
+        {
+            return new[] { "r", "o", "c", "a" };
+        }
+
+        }
+
+    }
+
+
+// Refactored solution
+
+
+
+/*******Look at Solution*******/
+// Second Refactored solution
+ public class BoggleBoard
+    {
+        private string[][] boggle_board = {
+             new[] {"b", "r", "a", "e"},
+             new[] {"i", "o", "d", "t"},
+             new[] {"e", "c", "l", "r"},
+             new[] {"t", "a", "k", "e"}
+        };
+
+        public string CreateWord(int[][] coords)
+        {
+            string returnString = "";
+            foreach (int[] letter in coords)
+            {
+                int x = letter[0];
+                int y = letter[1];
+                returnString += boggle_board[x][y];
+            }
+            return returnString;
+        }
+         
+
+        public string [] GetRow(int row) // create getrow method which takes single int input
+        {
+            return new[] { "i", "o", "d", "t" }; // return new string array from boggleboard
+        }
+
+        public string[] GetColumn(int column) // create getColumn method which takes single int input
+        {
+            return new[] { "r", "o", "c", "a" }; // return new string array from boggleboard
+        }
+
+        }
+
+
+// Reflections
+// At first I didn't read the question, which meant this challenge took me way longer than what it should. After taking a break
+// and coming back to it I was able to make the tests pass in a short time.
+// This lesson helped me in establishing in knowing when to take a shortbreak and come back to things.
+// I also gained knowledge that sometimes, short sharp code is the answer.
---
+++ Week 6 C# Challenges/GuessGameFinal.cs
@@ -0,0 +1,90 @@
+﻿/********************************* This File is not compiled or tested ********************************/
+// Pseudo-code
+
+
+
+// Initial solution
+namespace Phase0_Exercise30_Simple_Guessing_Game
+{
+    public class GuessingGame
+    {
+        int answer; // the Guessing game class will initialise the class member when an instance of the class is created in static void main()
+
+        public int Guess(int guess)
+        {
+            if (guess > answer)
+                return "high";
+
+            else if (guess == answer)
+                return "correct";
+
+            else if (guess < answer)
+                return "low";
+        }
+
+        public bool IsSolved()
+        {
+            if (lastGuess && lastResult == "correct")
+                return true;
+
+            else
+                return false;
+        }
+
+
+
+// Refactored solution
+namespace GuessingGame
+{
+    public class GuessGame
+    {
+
+        public int answer;
+        public int guess;
+
+
+        public GuessGame(int answer)
+        {
+            answer = 0;
+            guess = 0;
+        }
+
+        public int Guess(int guess)
+        {
+            if (guess > answer)
+                Console.WriteLine("high");
+
+            else if (guess == answer)
+                Console.WriteLine("correct");
+
+            else if (guess < answer)
+                Console.WriteLine("low");
+
+            return answer; // provides a return path if the code does not enter the if statement.
+
+        }
+
+        public bool IsSolved()
+        {
+            if (guess == answer)
+
+                return true;
+
+            else
+                return false;
+        }
+    }
+}
+
+
+/*******Look at Solution*******/
+// Second Refactored solution
+
+
+
+// Reflections
+//This challenge was an insightful challenge for me because it did taught me an important thing; there is a thin line
+// between bloody minded perserverance and knowing when you are stuck and have to ask for help.  It turns out there
+// was a small error with the tests, so additionally, sometimes it is better to go away, and 
+// come back the next day, as you actually do get insights when your mind is fresh and not worn down. 
+//
