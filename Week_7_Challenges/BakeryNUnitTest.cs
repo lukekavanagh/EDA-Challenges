5600ace725bf48df36d8992157e3fbb162211f1d
lukekavanagh <lukekavanagh1@yahoo.co.uk>
2015-03-27 11:03:25 +1300

    Week 7 Challenges

---
+++ Week_7_Challenges/BakeryNUnitTest.cs
@@ -0,0 +1,169 @@
+﻿/******************** Summary *************************
+Test multiple permutations of the use of the CalculateQuantities method.
+
+These tests are to determine the edge cases of the use of the method.
+
+There are five variations of a test for the CalculateQuantities method to write.
+
+All follow the pattern of 
+
+ * Arrange
+ * 
+ * Act
+ * 
+ * Assert
+ 
+Do not be tempted to copy paste the first one - this is an exercise to help build muscle memory.
+*******************************************************/
+
+using EdaBakery;
+using NUnit.Framework;
+using REPLTestSupport;
+
+namespace UnitTestProject
+{
+    //This is called an attribute - it provides information about the BakeryTest class.
+    //Here it says that the class is a Test Class using the NUnit test framework.
+    [TestFixture]
+    public class BakeryTest
+    {
+        //This is a class variable/field.
+        private Bakery bakery;
+
+        //The SetUp attribute tells NUnit to run this method before every test. It is responsible for setting up any requirements that all tests need.
+        //In this case as we are testing the Bakery class we want to create a new instance/version of it each time. This silos the tests.
+        //If we didn't a later test may give a false return if it is based on something a previous test had done. 
+        [SetUp]
+        public void init()
+        {
+            bakery = new Bakery();
+        }
+
+        //The Test Attribute tells NUnit that the method is a test.
+        [Test]
+        public void TestCalculateQuantitiesExists()
+        {
+            //Here we use a var for the type as we are not sure what type it is.
+            var actual = HelperMethods.getMethod(bakery.GetType(), "CalculateQuantities");
+
+            //Assert is a static class we use for asserting/testing the results of tests. Google "msdn assert" and look at the methods section.
+            Assert.NotNull(actual);
+        }
+        
+
+        //Each of the comments below will guide you through writing the test. Fill in the code under each of the comments.
+        //Start by creating a Test attribute.
+        [Test]
+        public void CalculateQuantities();
+        
+
+
+        //Now define a public method that returns void called TestResultOfCalculateQuantities1. 
+        //We have added the body tags ({} on line 50 and 65) for the method to make organising the comments easier. uncomment them when you have named the method.
+        [TestMethod]
+        public void TestResultofCalculateQuantities1();
+        {
+        //Arrange
+        public string expected = "You need to make 4 cakes";
+        //Define a string field/variable called expected and assign it (=) the value "You need to make 4 cake(s).". This is what we are expecting as a result of the method.
+
+        //Define a string field/variable called actual. this has no value at the moment. we will add one as an action below.
+        public string actual = "";
+
+        //Act
+        //Assign the result of using the CalculateQuantities with the arguments (24, "cake") to the field actual. Remember assignments go from right to left. example below.
+        //actual = methodThingee(9, "words of wonder");
+        var actual =  TestResultofCalculateQuantities1(24, "cake");
+        
+
+        //Assert
+        //Use the Assert class with the method AreEqual. Then pass the fields expected and actual in separated by a comma.
+        Assert.AreEqual(expected, actual);
+        }
+
+        //Once you have finished the test you should be able to run it and see it passing.
+
+        //Now write four more tests for these combinations:
+        /*
+		actual - CalculateQuantities(41, "pie") and expected - "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)."
+        */
+        [Test]
+        public void CalculateQuantities2();
+
+        [TestMethod]
+        public void TestResultofCalculateQuantities2();
+        {
+        public string expected = "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)."; //Arrange
+        
+        public string actual = ""; //Arrange
+
+        var actual = TestResultOfCalculateQuantities2(41, "pie"); // Act
+
+        Assert.AreEqual(expected, actual); //Assert
+
+	//	actual - CalculateQuantities(24, "cookie") and expected - "You need to make 24 cookie(s)."
+        [Test]
+        public void CalculateQuantities3();
+        
+        [TestMethod]
+        public void ResultOfCalculateQuantities3();
+        {
+        public string expected = "You need to make 24 cookie(s)."//Arrange
+
+        public string actual = ""; // Arrange
+
+        var actual = ResultOfCalculateQuantities3(24, "cookie"); //Act
+
+        Assert.AreEqual(expected, actual);
+
+        }
+        
+        // actual - CalculateQuantities(4, "pie") and expected - "You need to make 0 pie(s), 0 cake(s), and 4 cookie(s)."
+        [test]		
+        public void CalculateQuantities4();
+
+        [TestMethod]
+        public void ResultOfCalculatingQuantities4();
+        {
+        
+        public string expected = "You need to make 0 pie(s), 0 cake(s), and 4 cookie(s).";
+
+        public string actual = "";
+
+        var actual = ResultOfCalculatingQuantities4(4, "pie");
+
+        Assert.AreEqual(expected, actual);
+        }
+
+        //actual - CalculateQuantities(130, "pie") and expected - "You need to make 16 pie(s), 0 cake(s), and 2 cookie(s)."
+       
+        [Test]
+        public void CalculatingQuantities5();
+            
+        public void ResultOfCalculatingQuantities5();
+        {
+        public string expected = "You need to make 16 pie(s), 0 cake(s), and 2 cookie(s).";
+
+        public string actual = "";
+
+        var actual = ResultOfCalculatingQuantities5(130, "pie");
+
+        AssertAreEqual(expected, actual);
+        }
+        
+
+        
+
+
+
+
+
+
+
+
+
+         
+        When finished go back to the Bakery class and start refactoring! Remember to run your tests with each change.
+        */
+    }
+}
---
+++ Week_7_Challenges/DrawerDebugger.cs
@@ -0,0 +1,122 @@
+﻿/********************************* This File is not compiled or tested ********************************/
+//When your done copy paste Silverware, Drawer and Tests here
+// Drawer
+using System.Collections.Generic;
+
+namespace DrawerDebugger
+{
+    public class Drawer // create public class drawer
+    {
+        // Are there any more methods needed in this class?************
+        public List<Silverware> contents; // initialise public list called contents which references the silverware class as it's type
+
+        private bool open; // set private boolean property open
+
+        public Drawer() //creation of class Constructor which initialises two members
+        {
+            contents = new List<Silverware>();  //create and assign new List object called contents which references the silverware class
+            open = true; //assign private boolean open to the state true.
+        }
+
+        public void OpenDrawer() //create method with empty constructor opens the drawer, which assigns the open property to a true state
+        {
+            open = true; //set boolean property open to true when this method is invoked
+        }
+
+        public void Close() //create method with empty constructor which closes the drawer
+        {
+            open = false; // set boolean property open to false when this method is invoked.
+        }
+
+        public List<Silverware> AddItem(Silverware item) //create method Add Item which takes a public list of type silverware class as a type
+        {
+            for (int i = 0; i < contents.Count; i++)
+            {
+                item = contents[i];
+
+                if (item != null) break;
+            }
+            contents.Add(item);
+            return contents;
+            //return the contents list, with the addition of an item to this contents list.
+
+        }
+
+        public Silverware RemoveItem(Silverware item = null) //create Remove Item method, which references the silverware class, and sets a value called item to null as input
+        {
+            if (item == null) //if item == null
+            {
+                for (int i = 0; i < contents.Count; i++) //create for loop which returns a cached field of the contents.Count
+                {
+                    item = contents[i]; // what is this doing? // at each iteration, the value item  is assigned to the contents list at each iteration.
+                    // i is position at each iteration within contents list.
+                    if (item != null) break; //if item is not equal to null, break the for loop.
+                }
+            }
+
+            contents.Remove(item); //and remove an item from the contents list
+            return item; //return item, which is assigned to the contents list, so should return to the contents list?
+        }
+
+
+        public void Dump() // what should this method return? it's void it can't return anything? 
+        {
+            Console.WriteLine("Your drawer is now empty");//print message to the console
+        }
+
+        public void ViewContents() //create method ViewContents... 
+        {
+            Console.WriteLine("The drawer contains:"); //which prints this message to console
+            foreach (Silverware silverware in contents) //creates foreach which uses a reference to the silverware class and goes through the contents list..
+            {
+                Console.WriteLine("- " + silverware.type); //for each silverware reference print message to console
+            }                                              //and the silverware type, the reference to silverware is accessing the public property string called type in the silverware class
+        }
+    }
+}
+
+// Silverware
+
+namespace DrawerDebugger
+{
+    public class Silverware //create public class Silverware
+    {
+        public String type; // set two properties, one a public string type denoting type of silverware
+        public bool clean;  // set other property which is a public boolean to denote if the silverware is clean or not via true/false
+
+        public Silverware(String silverware_type, bool is_clean = true)
+        // create method silverware which takes a string silverware_type as input and a boolean which sets state to true, to denote that the silverware is clean
+        {
+            type = silverware_type; //assigns the type property to silverware_type so we can use it here and elsewhere
+            clean = is_clean; // assigns the property clean to is_clean
+        }
+
+        public void Eat() //creation of a method which returns void called Eat
+        {
+            Console.WriteLine("eating with the " + type); //print "eating with the" + the type property to console
+            clean = false; // clean is false when this method is called.
+        }
+
+        public void CleanSilverware()
+        {
+            Console.WriteLine("Cleaning SilverWare");
+            clean = true;
+        }
+
+
+    }
+}
+
+
+// Tests
+//Problem with Nunit - am speaking to Dan about it
+
+
+// Reflections
+// Enjoyed this challenge despite experiencing technical problems with the tests.
+// I have been timeboxing my challenges to two hours so I don't spend more time than what  I need to
+// ensuring I take what I can from the challenges and move on
+// I seem to have got around my head around how classes, properties and methods interact between each other so that is good.
+// Was a fun challenge to do and I enjoyed commenting on what each part of the code does, i tend to make this a habit in my challenges in phase zero
+// which means the challenges can take a bit of time
+// The Nunit framework  not working is disapointing, but I have to try to learn some more about the writing of tests.
